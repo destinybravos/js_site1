5212a25152bb4d12c0fccf34e10e8f7c91092dfe
destinybravos <destinybravos@gmail.com>
2022-11-22 18:04:18 +0100

    first commit

---
+++ js/scrpits.js
@@ -0,0 +1,9 @@
+// document.getElementById('msg').innerText = "Connected";
+// document.getElementsByName('lll')[0].innerText = "Connected";
+// document.querySelector('.lll').innerText = "Connected xssdsd";
+
+// Variable
+// var a = 12;
+// let b = "hello";
+// let c = true;
+// const PI = 3.142
